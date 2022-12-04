0c703a2b5edb861a3772b3b2b7e978e8703b6c5d
Zenza235 <paoloatienza235@gmail.com>
2022-12-04 16:15:04 -0500

    finished day 4 part 1

---
+++ day-4/part1.py
@@ -0,0 +1,16 @@
+import sys
+import re
+
+with open(f'{sys.argv[1]}.txt', 'r') as f:
+    input = f.read().splitlines()
+
+overlap_total = 0
+
+for a in input:
+    temp = re.split('-|,', a)
+    nums = [int(s) for s in temp]
+
+    if (nums[2] - nums[0]) * (nums[3] - nums[1]) <= 0:
+        overlap_total += 1
+
+print(overlap_total)
