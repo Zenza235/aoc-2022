4fbe3062e7c21d91de1a7f4494e664aadb8c9780
Zenza235 <paoloatienza235@gmail.com>
2022-12-04 14:54:58 -0500

    finished day 3 part 1

--- day-2/part1.py
+++ day-2/part1.py
@@ -1,6 +1,6 @@
-# A == rock        Y == paper (2 pts)
-# B == paper       Z == scissors (3 pts)
-# C == scissors    X == rock (1 pt)
+# A == rock        X == rock (1 pts)
+# B == paper       Y == paper (2 pts)
+# C == scissors    Z == scissors (3 pt)
 
 # Win == 6
 # Draw == 3
@@ -8,7 +8,24 @@
 
 import sys
 
+dict = {
+    'A X' : 4, # tie
+    'A Y' : 8, # win
+    'A Z' : 3, # loss
+    'B X' : 1, # loss
+    'B Y' : 5, # tie
+    'B Z' : 9, # win
+    'C X' : 7, # win
+    'C Y' : 2, # loss
+    'C Z' : 6 # tie
+}
+
 with open(f'{sys.argv[1]}.txt', 'r') as f:
     input = f.read().splitlines()
 
-print(input)
+total = 0
+
+for a in input:
+    total += dict[a]
+
+print(total)
