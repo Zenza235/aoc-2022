2b9169fdb04d30a87405adcb0d5d2a1c78f557b3
Zenza235 <paoloatienza235@gmail.com>
2022-12-04 15:57:53 -0500

    completed day 3

--- day-3/part2.py
+++ day-3/part2.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import sys
 
 with open(f'{sys.argv[1]}.txt', 'r') as f:
@@ -5,3 +6,21 @@ with open(f'{sys.argv[1]}.txt', 'r') as f:
 
 def get_priority(c: str) -> int:
     return ord(c) - 38 if c.isupper() else ord(c) - 96
+
+total = 0
+
+for i in range(0, len(input), 3):
+    r1 = input[i]
+    r2 = input[i + 1]
+    r3 = input[i + 2]
+
+    dict1 = Counter(r1)
+    dict2 = Counter(r2)
+    dict3 = Counter(r3)
+
+    commonchar = list((dict1 & dict2 & dict3).elements())
+
+    total += get_priority(commonchar[0])
+    
+
+print(total)
