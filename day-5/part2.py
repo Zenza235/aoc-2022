7860723696b3631105c191f5f1664347a5b85bd3
Zenza235 <paoloatienza235@gmail.com>
2022-12-05 11:00:29 -0500

    finished day 5

---
+++ day-5/part2.py
@@ -0,0 +1,50 @@
+import sys
+
+with open(f'{sys.argv[1]}.txt', 'r') as f:
+    input = f.read().splitlines()
+
+split = input.index('')
+instructions = input[split + 1 : ]
+stack_data = input[ : split - 1]
+num_stacks = int(input[split - 1].strip()[-1])
+
+sd = [a.split(' ') for a in stack_data]
+
+stacks = []
+
+for s in range(len(sd[0])):
+    stack = []
+    for i in range(len(sd) - 1, -1, -1):
+        str = sd[i][s]
+        if str != '':
+            stack.append(str[1])
+
+    stacks.append(stack)
+
+def do_ins(num_move: int, start: list, end: list) -> list:
+    if num_move > 1:
+        # do thing
+        vals = start[len(start) - num_move : len(start)]
+        del start[len(start) - num_move : len(start)]
+        end.extend(vals)
+    else:
+        # do normal thing
+        val = start.pop(len(start) - 1)
+        end.append(val)
+
+for ins in instructions:
+    words = ins.split(' ')
+    move_amt = int(words[1])
+    start_stack = int(words[3])
+    end_stack = int(words[5])
+
+    do_ins(move_amt, stacks[start_stack - 1], stacks[end_stack - 1])
+
+print(stacks)
+
+result = ''
+
+for stack in stacks:
+    result += stack[len(stack) - 1]
+
+print(result)
